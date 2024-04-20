43ba9f5702433b1855a7e2c599d2c29c2fa6e03a
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2024-04-20 12:46:58 +0200

    Dodata je implementacija Logger-a. Dodata je skripta vezana za bazu

--- ZakazivanjePregledaBekend/ZakazivanjePregledaAPI/Program.cs
+++ ZakazivanjePregledaBekend/ZakazivanjePregledaAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using ZakazivanjePregledaAPI.Extensions;
+
 namespace ZakazivanjePregledaAPI
 {
     public class Program
@@ -7,15 +11,38 @@ namespace ZakazivanjePregledaAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            LogManager.Setup().LoadConfiguration(config =>
+            {
+               
+            });
+            
+            builder.Services.ConfigureCORS();
+            builder.Services.ConfigureIISIntegration();
+            builder.Services.ConfigureLoggerService();
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
-            app.UseHttpsRedirection();
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            app.UseCors("ZakazivanjePregledaCORS");
             app.UseAuthorization();
 
 
